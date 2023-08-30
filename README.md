7b8980fcb8ad698c5f4d9bb3799029e647ce71b1
Ben G <bgree825@gmail.com>
2023-08-30 15:29:32 -0400

    convert to package

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # python-utilities
 
-A file meant to hold functions and classes that can be helpful to have included while working on other projects.
+A package containing functions and classes that can be helpful to have included while working on other projects.
---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(name='PUTILS',
+                 version='1.0.0',
+                 description='Python Utilities',
+                 url='#',
+                 author='Ben Greenfield',
+                 install_requires=[],
+                 packages=['PUTILS'])
